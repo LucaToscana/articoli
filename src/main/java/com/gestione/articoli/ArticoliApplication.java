cda0f6855bd85af56459b0a58f11a9a6796e7762
luca toscana <lucatscn@gmail.com>
2025-07-23 22:13:12 +0200

    Initial commit: Spring Boot base project

---
+++ src/main/java/com/gestione/articoli/ArticoliApplication.java
@@ -0,0 +1,13 @@
+package com.gestione.articoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticoliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestione/articoli/ArticoliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestione.articoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
